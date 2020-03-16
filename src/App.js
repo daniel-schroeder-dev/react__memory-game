655100af493158ea8a4f1805cf074bda9d9d31e9
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-16 14:06:54 -0500

    Add Card component and basic styling.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import Header from './Header';
+import Card from './Card';
 import './App.css';
+
+const cards = [];
+const NUM_CARDS = 16;
+
+for (let i = 0; i < NUM_CARDS; i++) {
+  cards.push(<Card />);
+}
 
 function App() {
   return (
-    <div className="app-container">
+    <div className="app">
       <Header />
+      <div className="card-container">
+        {cards}
+      </div>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -3,10 +3,10 @@ import './Header.css';
 
 function Header(props) {
   return (
-    <header className="header-container">
-      <h1 className="header-container__page-title">Memory Game</h1>
+    <header className="header">
+      <h1 className="header__page-title">Memory Game</h1>
       <nav>
-        <ul className="header-container__nav-items">
+        <ul className="header__nav-items">
           <li>New Game</li>
         </ul>
       </nav>
