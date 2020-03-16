fa0979ceac629fd4dd51c525bd0ba94ce33285e1
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-16 14:19:35 -0500

    Refactor App to be a class component.

--- src/App.js
+++ src/App.js
@@ -3,31 +3,48 @@ import Header from './Header';
 import Card from './Card';
 import './App.css';
 
-const cards = [];
-const NUM_CARDS = 16;
+class App extends React.Component {
 
-for (let i = 0; i < NUM_CARDS; i++) {
-  cards.push(<Card key={i} />);
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: this.createCards(),
+    }
+  }
 
-function App() {
-  return (
-    <div className="app">
-      <Header />
-      <div className="card-container">
-        {cards}
+  createCards = () => {
+
+    const cards = [];
+    const NUM_CARDS = 16;
+
+    for (let i = 0; i < NUM_CARDS; i++) {
+      cards.push(<Card key={i} />);
+    }
+
+    return cards;
+  
+  };
+  
+  render() {
+    return (
+      <div className="app">
+        <Header />
+        <div className="card-container">
+          {this.state.cards}
+        </div>
+        <div className="instructions">
+          <h2>Welcome to the Memory Game!</h2>
+          <p>Rules for gameplay:</p>
+          <ol>
+            <li>Click a card to reveal the color of the card.</li>
+            <li>Click another card, if the colors are the same, the colors will stay revealed. If the colors are different, both cards will return back to a grey color.</li>
+            <li>The game is finished when all of the cards have been successfully revealed.</li>
+          </ol>
+        </div>
       </div>
-      <div className="instructions">
-        <h2>Welcome to the Memory Game!</h2>
-        <p>Rules for gameplay:</p>
-        <ol>
-          <li>Click a card to reveal the color of the card</li>
-          <li>Click another card, if the colors are the same, the colors will stay revealed. If the colors are different, both cards will return back to a grey color.</li>
-          <li>The game is finished when all of the cards have been successfully revealed</li>
-        </ol>
-      </div>
-    </div>
-  );
+    );  
+  }
+  
 }
 
 export default App;
