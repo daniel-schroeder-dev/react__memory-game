d369be35d2147e9506f2473febdb502d1d6da3c5
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-16 18:14:14 -0500

    Updates

---
+++ precache-manifest.5b483b9dca5b7f50cab0800ad0a8a798.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea576ae88a3488392e4b8ee1a9b3dc01",
+    "url": "/react__memory-game/index.html"
+  },
+  {
+    "revision": "68366ebf1dd97209e082",
+    "url": "/react__memory-game/static/css/main.5f37fc35.chunk.css"
+  },
+  {
+    "revision": "fb3611756e0e9f1ee718",
+    "url": "/react__memory-game/static/js/2.679d9c41.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react__memory-game/static/js/2.679d9c41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68366ebf1dd97209e082",
+    "url": "/react__memory-game/static/js/main.cc40ea3a.chunk.js"
+  },
+  {
+    "revision": "ec127eb805b3e98437fe",
+    "url": "/react__memory-game/static/js/runtime-main.d03e3561.js"
+  }
+]);
