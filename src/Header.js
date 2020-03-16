4ad257cb97c807be25ec0a193e87a267063e45a6
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-16 13:52:54 -0500

    Add basic Header component.

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header(props) {
+  return (
+    <header>
+      <h1>Memory Game</h1>
+      <nav>
+        <ul>
+          <li>New Game</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
